939669a3e56889a8d38e8676ea6b866b582a75f3
unknown <derek.slauson5@gmail.com>
2022-05-22 14:55:10 -0500

    Simple theme toggle is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react"
+import "./scss/_main.scss"
+
 function App() {
+
+  const [theme, setTheme] = useState("light")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className={theme}>
+      <div className="background flex">
+        <h1 className="text">Hello</h1>
+        <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>Switch Theme</button>
+      </div>
     </div>
   )
 }
